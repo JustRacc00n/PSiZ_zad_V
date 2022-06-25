20fe9a382213fbcd4a4eca73e8db98b52574547c
JustRacc00n <barteklew123@gmail.com>
2022-06-25 17:36:22 +0200

    Dodaj pliki projektów.

---
+++ PSiZ5/PSiZ5.cpp
@@ -0,0 +1,54 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+struct PlikHeader {
+    short Type;
+    int Size;
+    short Reserved1;
+    short Reserved2;
+    int OffBits;
+} Plik;
+
+struct ObrazHeader {
+    int headerSize;
+    int width;
+    int height;
+    short int planes;
+    short int bitPerPixel;
+    int compresion;
+    int imageSize;
+    int xPelsPerMeter;
+    int yPelsPerMeter;
+    int colorUsed;
+    int colorImportant;
+} Obraz;
+
+struct KoloryRGB {
+    char R;
+    char G;
+    char B;
+} Rgb;
+
+void Informacje()
+{
+
+}
+
+void Negatyw()
+{
+
+}
+
+int main(int arc, char* argv[]) 
+{
+
+
+
+
+    return 0;
+}
